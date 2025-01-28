94f88601f427747a9984c8c9144475b9f0901cbd
Alua <aluuua9@gmail.com>
2025-01-28 23:54:48 +0500

    functions14

---
+++ lab3/main.py
@@ -0,0 +1,9 @@
+from functions import is_palindrome, guess_the_number
+
+word = input("Enter a word or phrase to check if it's a palindrome: ")
+if is_palindrome(word):
+    print(f"{word} is a palindrome!")
+else:
+    print(f"{word} is not a palindrome!")
+
+guess_the_number()
